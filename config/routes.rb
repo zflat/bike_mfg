576ad3002cedbcf41331933886882b55479814d4
William Wedler <wwedler@riseup.net>
2012-12-28 20:19:48 -0500

    remove index action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :bike_mfg_queries, :only=>[:index] do
+      resources :bike_mfg_queries, :only=>[] do
         collection do
           get 'search'
           get 'search_models'
