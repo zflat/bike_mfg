b5e9680bba3916396c05b78cc8f4a92ecc7d0490
William Wedler <wwedler@riseup.net>
2012-12-28 20:47:15 -0500

    make json default format for api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api do
+  namespace :api, :defaults=>{:format => 'json'} do
     namespace :v1 do
       resources :bike_mfg_queries, :only=>[] do
         collection do
