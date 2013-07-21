df646ea068739a748b188a19002a6e3a7ffe69d1
William Wedler <wwedler@riseup.net>
2013-07-21 11:28:32 -0400

    loading rails engine in rails4

--- lib/bike_mfg/engine.rb
+++ lib/bike_mfg/engine.rb
@@ -1,6 +1,6 @@
 module BikeMfg
   class Engine < ::Rails::Engine
-    root = File.expand_path('../../../', __FILE__)
+    root = File.expand_path('../../', __FILE__)
     config.autoload_paths << root
   end
 end
